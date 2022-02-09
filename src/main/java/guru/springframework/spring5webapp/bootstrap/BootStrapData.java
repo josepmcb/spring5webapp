af78ca67ca98364ca1b4e557338d5901ab3ddf0c
josepmcb <josepmcb@gmail.com>
2022-02-09 16:37:45 +0100

    Afegit publisher

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -5,27 +5,34 @@ import org.springframework.stereotype.Component;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class BootStrapData implements CommandLineRunner {
 	
 	private final AuthorRepository authorRepository;
 	private final BookRepository bookRepository;
-	
-	
+	private final PublisherRepository publisherRepository;
 
-	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+
+
+	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+			PublisherRepository publisherRepository) {
 		super();
 		this.authorRepository = authorRepository;
 		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
 	}
-
 
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		System.out.println("Started in Bootstrap");
+
 		
 		Author eric = new Author("Eric", "Evans");
 		Book ddd = new Book("Domain Driven Design", "123123");
@@ -45,9 +52,15 @@ public class BootStrapData implements CommandLineRunner {
 		authorRepository.save(rod);
 		bookRepository.save(noEJB);
 		
-		System.out.println("Started in Bootstrap");
 		System.out.println("Number of books :" + bookRepository.count());
 		
+		Publisher publisher = new Publisher();
+		publisher.setName("Edicions 62");
+		publisher.setCity("Barcelona");
+		publisher.setState("Barcelona");
+		publisherRepository.save(publisher);
+		
+		System.out.println("Publisher count : " + publisherRepository.count());
 	}
 
 }
